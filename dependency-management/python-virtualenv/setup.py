ecda8ca587747711624c4b826b4ab8ebef4d9572
Tom Busby <tom@flyfreeweb.co.uk>
2016-05-19 19:04:21 +0200

    Basic tox env/testing automation example

---
+++ dependency-management/python-virtualenv/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Toxtest',
+      version='0.1.0',
+      description='Tox test',
+      author='Tom Busby',
+      author_email='tom@busby.ninja',
+     )
